2ccfb7c77b39dee176dca69896c59bda3a19a010
Cardinal <cardinalby@gmail.com>
2022-02-04 10:59:11 +0300

    1.0.16

--- src/index.ts
+++ src/index.ts
@@ -7,4 +7,5 @@ export {FirefoxAddonsBuildResult, FirefoxAddonsExtIdAsset} from './buildResult';
 export {UnauthorizedError} from './errors/UnauthorizedError';
 export {ValidationError} from './errors/ValidationError';
 export {SameVersionAlreadyUploadedError} from './errors/SameVersionAlreadyUploadedError';
+export {PollTimedOutError} from './errors/PollTimedOutError';
 
